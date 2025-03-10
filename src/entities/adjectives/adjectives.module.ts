3e02b8d3404786f34f02e315cce51b4742aebe11
Me6111 <maksymmpawlowski@gmail.com>
2025-03-10 11:19:24 +0000

    Updated app.js

--- src/entities/adjectives/adjectives.module.ts
+++ src/entities/adjectives/adjectives.module.ts
@@ -1,12 +1,16 @@
+// src/entities/adjectives/adjectives.module.ts
+
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdjectiveController } from './adjectives.controller';
 import { AdjectivesService } from './adjectives.service';
 import { Adjectives } from './adjectives.entity';
+import { AdjectivesRepository } from './adjectives.repository'; // Import the repository
 
 @Module({
   imports: [TypeOrmModule.forFeature([Adjectives])],
   controllers: [AdjectiveController],
-  providers: [AdjectivesService],
+  providers: [AdjectivesService, AdjectivesRepository], // Provide the repository
+  exports: [AdjectivesRepository], // Export the repository
 })
 export class AdjectivesModule {}
