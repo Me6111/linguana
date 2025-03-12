355358b8b06011d347bbe58cb2b473b24fe38fdb
Me6111 <maksymmpawlowski@gmail.com>
2025-03-12 22:22:36 +0000

    Updated app.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,12 @@ import { AdjectivesModule } from './entities/adjectives/adjectives.module';
 import { NounsModule } from './entities/nouns/nouns.module';
 import { SyncController } from './sync/sync.controller';
 import { SyncService } from './sync/sync.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Adjectives } from './entities/adjectives/adjectives.entity';
+import { Nouns } from './entities/nouns/nouns.entity';
 
 @Module({
-  imports: [DatabaseModule, AdjectivesModule, NounsModule],
+  imports: [DatabaseModule, AdjectivesModule, NounsModule, TypeOrmModule.forFeature([Adjectives, Nouns])],
   controllers: [AppController, SyncController],
   providers: [AppService, SyncService],
 })
