258a663b4a7bf107d979fad50aab3336101cb2c1
Me6111 <maksymmpawlowski@gmail.com>
2025-03-30 23:53:14 +0100

    Updated app.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,13 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './data-source';
-import { SyncModule } from './sync/sync.module'; // Import SyncModule
+import { SyncModule } from './sync/sync.module';
+import { TableNamesService } from './table-names.service'; // Import TableNamesService
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot(dataSourceOptions),
-    SyncModule, // Add SyncModule to imports
-  ],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), SyncModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TableNamesService], // Add TableNamesService to providers
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
